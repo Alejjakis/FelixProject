b33360679187561caf6cd0a7c3b9024d7b10e9fc
Alejandro Felix Araujo <113490057+Alejjakis@users.noreply.github.com>
2022-09-26 21:07:08 -0600

    Add files via upload

---
+++ tipos de datos.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    std::cout<<"bool:\t\t";<< sizeof(bool)<< "bytes\n";
+    std::cout<<"char:\t\t"<<sizeof(char<<"bytes\n");
+    std::cout<<"wchar_t:\t"<<sizeof(char)<<"bytes\n";
+    std::cout<<"charr16_t\t"<<sizeof(charr16_t)<<"bytes/n";
+    std::cout<<"charr32_t\t"<<sizeof(charr132_t)<<"bytes/n";
+    std::cout<<"short:\t\t"<<sizeof(short)<<"bytes/n";
+    std::cout<<"int:\t\t"<<sizeof(int)<<"bytes/n";
+    std::cout<<"long:\t"<<sizeof(long)<<"bytes/n";
+    std::cout<<"long long:\t"<<sizeof(long)<<"bytes/n";
+    std::cout<<"float:\t\t"<<sizeof(float)<<"bytes/n";
+    std::cout<<"double:\t\t"<<sizeof(double)<<"bytes/n";
+    std::cout<<"long double:\t"<<sizeof(long double)<<"bytes/n";
+    
+    return 0;
+}
